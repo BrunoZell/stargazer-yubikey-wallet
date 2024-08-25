d9ebeb8353fab90e2db788c24015a1f70aa44587
Bruno <ask@bruno.wtf>
2024-08-25 23:16:12 +0200

    create non-existent directory

--- wallet-host-installer/install.js
+++ wallet-host-installer/install.js
@@ -26,15 +26,23 @@ function runNpmCommands(callback) {
 
 function installOnWindows() {
     const registryPath = 'HKLM\\SOFTWARE\\Google\\Chrome\\NativeMessagingHosts\\com.constellation.yubikey';
-    const jsonFilePath = path.join('C:\\Program Files\\YubikeyWallet\\native-messaging-host.json');
+    const manifestJsonPath = path.join('C:\\Program Files\\YubikeyWallet\\native-messaging-host.json');
     const executablePath = path.join(__dirname, '../dist', 'yubikey-wallet.exe');
 
     manifestJson.path = 'C:\\Program Files\\YubikeyWallet\\yubikey-wallet.exe';
     const jsonString = JSON.stringify(manifestJson, null, 2);
 
-    fs.writeFileSync(jsonFilePath, jsonString);
+    // Ensure the directory exists
+    fs.mkdirSync(path.dirname(manifestJsonPath), { recursive: true });
+
+    // Write manifest json so Chrome recognizes the Wallet as a Native Messaging Host
+    fs.writeFileSync(manifestJsonPath, jsonString);
+
+    // Copy the executable to the path
     fs.copyFileSync(executablePath, manifestJson.path);
-    exec(`reg add "${registryPath}" /ve /t REG_SZ /d "${jsonFilePath}" /f`, (error, stdout, stderr) => {
+
+    // Add the registry key
+    exec(`reg add "${registryPath}" /ve /t REG_SZ /d "${manifestJsonPath}" /f`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error adding registry key: ${error.message}`);
             return;
@@ -44,16 +52,22 @@ function installOnWindows() {
 }
 
 function installOnMacLinux() {
-    const jsonFilePath = path.join(os.homedir(), '.config', 'google-chrome', 'NativeMessagingHosts', 'com.constellation.yubikey.json');
+    const manifestJsonPath = path.join(os.homedir(), '.config', 'google-chrome', 'NativeMessagingHosts', 'com.constellation.yubikey.json');
     const executablePath = path.join(__dirname, '../dist', 'yubikey-wallet');
 
     manifestJson.path = '/usr/local/bin/yubikey-wallet';
     const jsonString = JSON.stringify(manifestJson, null, 2);
 
-    fs.mkdirSync(path.dirname(jsonFilePath), { recursive: true });
-    fs.writeFileSync(jsonFilePath, jsonString);
+    // Ensure the directory exists
+    fs.mkdirSync(path.dirname(manifestJsonPath), { recursive: true });
+
+    // Write manifest json so Chrome recognizes the Wallet as a Native Messaging Host
+    fs.writeFileSync(manifestJsonPath, jsonString);
+
+    // Copy the executable to the path
     fs.copyFileSync(executablePath, manifestJson.path);
-    console.log('JSON file and executable placed successfully');
+
+    console.log('Manifest JSON and executable placed successfully');
 }
 
 function install() {
