9221ddef94cf0c6e9ee05e66dfa0c4ff3f5a671b
Bruno <ask@bruno.wtf>
2024-08-25 22:57:36 +0200

    install paths

--- wallet-host-installer/install.js
+++ wallet-host-installer/install.js
@@ -3,17 +3,15 @@ const path = require('path');
 const os = require('os');
 const { exec } = require('child_process');
 
-const jsonContent = {
+const manifestJson = {
     "name": "com.constellation.yubikey",
     "description": "Native Messaging Host for Yubikey Wallet",
-    "path": "/path/to/native-messaging-host",
+    "path": "",  // This will be set dynamically based on the platform
     "type": "stdio",
     "allowed_origins": [
         "chrome-extension://dacabonkdgpbkfdmpnnngmgdcadpglla/"
     ]
 };
-
-const jsonString = JSON.stringify(jsonContent, null, 2);
 
 function runNpmCommands(callback) {
     exec('npm install && npm run build', { cwd: path.resolve(__dirname, '../wallet-host') }, (error, stdout, stderr) => {
@@ -28,11 +26,14 @@ function runNpmCommands(callback) {
 
 function installOnWindows() {
     const registryPath = 'HKLM\\SOFTWARE\\Google\\Chrome\\NativeMessagingHosts\\com.constellation.yubikey';
-    const jsonFilePath = path.join('C:\\path\\to\\native-messaging-host.json');
+    const jsonFilePath = path.join('C:\\Program Files\\YubikeyWallet\\native-messaging-host.json');
     const executablePath = path.join(__dirname, '../dist', 'yubikey-wallet.exe');
 
+    manifestJson.path = 'C:\\Program Files\\YubikeyWallet\\yubikey-wallet.exe';
+    const jsonString = JSON.stringify(manifestJson, null, 2);
+
     fs.writeFileSync(jsonFilePath, jsonString);
-    fs.copyFileSync(executablePath, 'C:\\path\\to\\native-messaging-host.exe');
+    fs.copyFileSync(executablePath, manifestJson.path);
     exec(`reg add "${registryPath}" /ve /t REG_SZ /d "${jsonFilePath}" /f`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error adding registry key: ${error.message}`);
@@ -46,9 +47,12 @@ function installOnMacLinux() {
     const jsonFilePath = path.join(os.homedir(), '.config', 'google-chrome', 'NativeMessagingHosts', 'com.constellation.yubikey.json');
     const executablePath = path.join(__dirname, '../dist', 'yubikey-wallet');
 
+    manifestJson.path = '/usr/local/bin/yubikey-wallet';
+    const jsonString = JSON.stringify(manifestJson, null, 2);
+
     fs.mkdirSync(path.dirname(jsonFilePath), { recursive: true });
     fs.writeFileSync(jsonFilePath, jsonString);
-    fs.copyFileSync(executablePath, path.join(os.homedir(), 'path', 'to', 'native-messaging-host'));
+    fs.copyFileSync(executablePath, manifestJson.path);
     console.log('JSON file and executable placed successfully');
 }
 
